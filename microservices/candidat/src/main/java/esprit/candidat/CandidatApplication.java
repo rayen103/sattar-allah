847ac6e3cbcc905c740235f6e8ed90fe59861d3f
Badiaa Bouhdid <badiaabouhdid@gmail.com>
2025-02-03 14:50:50 +0100

    microservice candidat created

---
+++ microservices/candidat/src/main/java/esprit/candidat/CandidatApplication.java
@@ -0,0 +1,13 @@
+package esprit.candidat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandidatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandidatApplication.class, args);
+	}
+
+}
